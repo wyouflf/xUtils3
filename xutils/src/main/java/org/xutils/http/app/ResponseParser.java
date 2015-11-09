90f5b5d618556878c14e65506248f5b245e61013
jiaolei <jiaolei@youlin.cn>
2015-11-09 18:26:49 +0800

    update javadoc

--- xutils/src/main/java/org/xutils/http/app/ResponseParser.java
+++ xutils/src/main/java/org/xutils/http/app/ResponseParser.java
@@ -12,5 +12,14 @@ public interface ResponseParser {
 
     void checkResponse(UriRequest request) throws Throwable;
 
+    /**
+     * 转换result为resultType类型的对象
+     *
+     * @param resultType  返回值类型(可能带有泛型信息)
+     * @param resultClass 返回值类型
+     * @param result      字符串数据
+     * @return
+     * @throws Throwable
+     */
     Object parse(Type resultType, Class<?> resultClass, String result) throws Throwable;
 }

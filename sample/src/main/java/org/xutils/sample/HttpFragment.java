cce1c3652dae30ed84fc177ca29a98baef3f1115
jiaolei <jiaolei@youlin.cn>
2015-11-14 16:50:19 +0800

    update doc

--- sample/src/main/java/org/xutils/sample/HttpFragment.java
+++ sample/src/main/java/org/xutils/sample/HttpFragment.java
@@ -212,16 +212,23 @@ public class HttpFragment extends BaseFragment {
             @Override
             public boolean onCache(String result) {
                 // 得到缓存数据
+                //
+                // * 客户端会根据服务端返回的 header 中 max-age 或 expires 来确定本地缓存是否给 onCache 方法.
+                //   如果服务端没有返回 max-age 或 expires, 那么缓存将一直保存, 除非这里自己定义了返回false的
+                //   逻辑, 那么xUtils将请求新数据, 来覆盖它.
+                //
                 // * 如果信任该缓存返回 true, 将不再请求网络;
                 //   返回 false 继续请求网络, 但会在请求头中加上ETag, Last-Modified等信息,
-                //   如果服务端返回304, 则表示数据没有更新, 不继续请求数据.
-                // * 客户端会根据服务端返回的 header 中 max-age 或 expires 来确定本地缓存是否给 onCache 方法.
+                //   如果服务端返回304, 则表示数据没有更新, 不继续加载数据.
+                //
                 this.result = result;
-                return false;
+                return false; // true: 信任缓存数据; false不信任缓存数据.
             }
 
             @Override
             public void onSuccess(String result) {
+                // 注意: 如果服务返回304或 onCache 选择了信任缓存, 这里将不会被调用,
+                // 但是 onFinished 总会被调用.
                 this.result = result;
             }
 

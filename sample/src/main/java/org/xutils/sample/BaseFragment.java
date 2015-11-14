1ba99b37443c94c6f265a8927bc3630dda5149ec
jiaolei <jiaolei@youlin.cn>
2015-11-15 00:32:13 +0800

    update sample

--- sample/src/main/java/org/xutils/sample/BaseFragment.java
+++ sample/src/main/java/org/xutils/sample/BaseFragment.java
@@ -13,9 +13,19 @@ import org.xutils.x;
  */
 public class BaseFragment extends Fragment {
 
+    private boolean injected = false;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        injected = true;
         return x.view().inject(this, inflater, container);
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if (!injected) {
+            x.view().inject(this, this.getView());
+        }
+    }
 }

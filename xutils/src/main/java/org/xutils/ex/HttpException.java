e86d84632c607dff23d44a32333246f368d803bf
jiaolei <jiaolei@youlin.cn>
2015-11-19 14:37:06 +0800

    update HttpException

--- xutils/src/main/java/org/xutils/ex/HttpException.java
+++ xutils/src/main/java/org/xutils/ex/HttpException.java
@@ -15,10 +15,13 @@
 
 package org.xutils.ex;
 
+import android.text.TextUtils;
+
 public class HttpException extends BaseException {
     private static final long serialVersionUID = 1L;
 
     private int code;
+    private String customMessage;
     private String result;
 
     public HttpException() {
@@ -71,11 +74,24 @@ public class HttpException extends BaseException {
         this.code = code;
     }
 
+    public void setMessage(String message) {
+        this.customMessage = message;
+    }
+
     /**
      * @return The http response status code, 0 if the http request error and has no response.
      */
     public int getCode() {
         return code;
+    }
+
+    @Override
+    public String getMessage() {
+        if (!TextUtils.isEmpty(customMessage)) {
+            return customMessage;
+        } else {
+            return super.getMessage();
+        }
     }
 
     public String getResult() {

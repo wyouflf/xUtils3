752f5e03b674f4ec1e7f8b6ff0a47b8f3de26786
jiaolei <jiaolei@youlin.cn>
2016-01-15 23:54:21 +0800

    merge: 优化注入错误提示

--- xutils/src/main/java/org/xutils/view/ViewInjectorImpl.java
+++ xutils/src/main/java/org/xutils/view/ViewInjectorImpl.java
@@ -160,7 +160,7 @@ public final class ViewInjectorImpl implements ViewInjector {
                             field.setAccessible(true);
                             field.set(handler, view);
                         } else {
-                            throw new RuntimeException("Invalid @ViewInject for"
+                            throw new RuntimeException("Invalid @ViewInject for "
                                     + handlerType.getSimpleName() + "." + field.getName());
                         }
                     } catch (Throwable ex) {

0992cbf3d21e586adfe43852535bf5c871013768
jiaolei <jiaolei@youlin.cn>
2016-01-23 00:24:13 +0800

    update sample

--- sample/src/main/java/org/xutils/sample/ImageFragment.java
+++ sample/src/main/java/org/xutils/sample/ImageFragment.java
@@ -52,7 +52,7 @@ public class ImageFragment extends BaseFragment {
                 .setSize(DensityUtil.dip2px(120), DensityUtil.dip2px(120))
                 .setRadius(DensityUtil.dip2px(5))
                 // 如果ImageView的大小不是定义为wrap_content, 不要crop.
-                .setCrop(true)
+                .setCrop(true) // 很多时候设置了合适的scaleType也不需要它.
                 // 加载中或错误图片的ScaleType
                 //.setPlaceholderScaleType(ImageView.ScaleType.MATRIX)
                 .setImageScaleType(ImageView.ScaleType.CENTER_CROP)

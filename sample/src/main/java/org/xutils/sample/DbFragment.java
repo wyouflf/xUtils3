236090d22d1aebe16e3d702dfbb1c6911adae842
jiaolei <jiaolei@youlin.cn>
2016-01-26 23:01:18 +0800

    update sample

--- sample/src/main/java/org/xutils/sample/DbFragment.java
+++ sample/src/main/java/org/xutils/sample/DbFragment.java
@@ -30,6 +30,13 @@ public class DbFragment extends BaseFragment {
             // 不设置dbDir时, 默认存储在app的私有目录.
             .setDbDir(new File("/sdcard")) // "sdcard"的写法并非最佳实践, 这里为了简单, 先这样写了.
             .setDbVersion(2)
+            .setDbOpenListener(new DbManager.DbOpenListener() {
+                @Override
+                public void onDbOpened(DbManager db) {
+                    // 开启WAL, 对写入加速提升巨大
+                    db.getDatabase().enableWriteAheadLogging();
+                }
+            })
             .setDbUpgradeListener(new DbManager.DbUpgradeListener() {
                 @Override
                 public void onUpgrade(DbManager db, int oldVersion, int newVersion) {
@@ -37,6 +44,8 @@ public class DbFragment extends BaseFragment {
                     // db.addColumn(...);
                     // db.dropTable(...);
                     // ...
+                    // or
+                    // db.dropDb();
                 }
             });
 

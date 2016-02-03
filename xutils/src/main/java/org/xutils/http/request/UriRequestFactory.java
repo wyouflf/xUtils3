9b6cfecb85840aecc9c080594ca7529784910c90
jiaolei <jiaolei@youlin.cn>
2016-02-03 11:46:28 +0800

    update UriRequestFactory

--- xutils/src/main/java/org/xutils/http/request/UriRequestFactory.java
+++ xutils/src/main/java/org/xutils/http/request/UriRequestFactory.java
@@ -1,11 +1,14 @@
 package org.xutils.http.request;
 
+import android.text.TextUtils;
+
 import org.xutils.common.util.LogUtil;
 import org.xutils.http.RequestParams;
 import org.xutils.http.app.RequestTracker;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Type;
+import java.util.HashMap;
 
 /**
  * Created by wyouflf on 15/11/4.
@@ -14,25 +17,43 @@ import java.lang.reflect.Type;
 public final class UriRequestFactory {
 
     private static Class<? extends RequestTracker> defaultTrackerCls;
-    private static Class<? extends AssetsRequest> assetsRequestCls;
+
+    private static final HashMap<String, Class<? extends UriRequest>>
+            SCHEME_CLS_MAP = new HashMap<String, Class<? extends UriRequest>>();
 
     private UriRequestFactory() {
     }
 
     public static UriRequest getUriRequest(RequestParams params, Type loadType) throws Throwable {
+
+        // get scheme
+        String scheme = null;
         String uri = params.getUri();
-        if (uri.startsWith("http")) {
-            return new HttpRequest(params, loadType);
-        } else if (uri.startsWith("assets://")) {
-            if (assetsRequestCls != null) {
-                Constructor<? extends AssetsRequest> constructor
-                        = assetsRequestCls.getConstructor(RequestParams.class, Class.class);
+        int index = uri.indexOf(":");
+        if (index > 0) {
+            scheme = uri.substring(0, index);
+        } else if (uri.startsWith("/")) {
+            scheme = "file";
+        }
+
+        // get UriRequest
+        if (!TextUtils.isEmpty(scheme)) {
+            Class<? extends UriRequest> cls = SCHEME_CLS_MAP.get(scheme);
+            if (cls != null) {
+                Constructor<? extends UriRequest> constructor
+                        = cls.getConstructor(RequestParams.class, Class.class);
                 return constructor.newInstance(params, loadType);
             } else {
-                return new AssetsRequest(params, loadType);
+                if (scheme.startsWith("http")) {
+                    return new HttpRequest(params, loadType);
+                } else if (scheme.equals("assets")) {
+                    return new AssetsRequest(params, loadType);
+                } else if (scheme.equals("file")) {
+                    return new LocalFileRequest(params, loadType);
+                } else {
+                    throw new IllegalArgumentException("The url not be support: " + uri);
+                }
             }
-        } else if (uri.startsWith("file:") || uri.startsWith("/")) {
-            return new LocalFileRequest(params, loadType);
         } else {
             throw new IllegalArgumentException("The url not be support: " + uri);
         }
@@ -51,7 +72,7 @@ public final class UriRequestFactory {
         return null;
     }
 
-    public static void registerAssetsRequestClass(Class<? extends AssetsRequest> assetsRequestCls) {
-        UriRequestFactory.assetsRequestCls = assetsRequestCls;
+    public static void registerRequestClass(String scheme, Class<? extends UriRequest> uriRequestCls) {
+        SCHEME_CLS_MAP.put(scheme, uriRequestCls);
     }
 }

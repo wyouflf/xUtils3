6c7ae374651c849f900d72a05ba75f12db55ae02
wyouflf <wyouflf@qq.com>
2016-06-13 01:36:35 +0800

    update IOUtil

--- xutils/src/main/java/org/xutils/common/util/IOUtil.java
+++ xutils/src/main/java/org/xutils/common/util/IOUtil.java
@@ -59,23 +59,19 @@ public class IOUtil {
         }
     }
 
-    public static byte[] readBytes(InputStream in, long skip, long size) throws IOException {
-        ByteArrayOutputStream out = null;
-        try {
-            if (skip > 0) {
-                long skipSize = 0;
-                while (skip > 0 && (skipSize = in.skip(skip)) > 0) {
-                    skip -= skipSize;
-                }
+    public static byte[] readBytes(InputStream in, long skip, int size) throws IOException {
+        byte[] result = null;
+        if (skip > 0) {
+            long skipped = 0;
+            while (skip > 0 && (skipped = in.skip(skip)) > 0) {
+                skip -= skipped;
             }
-            out = new ByteArrayOutputStream();
-            for (int i = 0; i < size; i++) {
-                out.write(in.read());
-            }
-            return out.toByteArray();
-        } finally {
-            closeQuietly(out);
         }
+        result = new byte[size];
+        for (int i = 0; i < size; i++) {
+            result[i] = (byte) in.read();
+        }
+        return result;
     }
 
     public static String readStr(InputStream in) throws IOException {

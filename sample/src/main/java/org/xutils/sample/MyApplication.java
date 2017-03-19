c10ff09438e5bece42cf693c30e67aec2fe34505
wyouflf <wyouflf@qq.com>
2017-03-19 13:06:26 +0800

    update sample

--- sample/src/main/java/org/xutils/sample/MyApplication.java
+++ sample/src/main/java/org/xutils/sample/MyApplication.java
@@ -19,7 +19,7 @@ public class MyApplication extends Application {
         x.Ext.init(this);
         x.Ext.setDebug(BuildConfig.DEBUG); // 开启debug会影响性能
 
-        // 信任所有https域名
+        // 信任所有https域名 或 仅添加信任的https域名
         /*HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
             @Override
             public boolean verify(String hostname, SSLSession session) {
